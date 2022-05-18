ffe4eba0d94722758ea564c091bab8e520687664
BlerimShabani <92629782+BlerimShabani@users.noreply.github.com>
2022-05-18 11:46:26 +0200

    Navigation

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function App() {
+import Dashboard from "./components/Dashboard";
+import Skills from "./components/Skills";
+import Notifications from "./components/Notifications";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const BottomNavigator=()=> {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, focused }) => {
+            let iconName;
+
+            if (route.name === "Dashboard") {
+              // iconName = focused
+              //   ? "home-sharp" 
+              //   : "ios-information-circle-outline";
+                return <Ionicons name={
+                  "home-sharp" 
+                   } size={25} color={color} />;
+              // iconName = 'home-sharp';
+            } else if (route.name === "ListOffers") {
+              
+              return <MaterialCommunityIcons name="text-box-search-outline" size={24} color={color} />
+              // iconName = "ios-options";
+            } else if (route.name === "Notification") {
+              // iconName = "notifications-sharp";
+              return <Ionicons  name={"notifications-sharp"} size={25} color={color} />;
+              
+            } else if (route.name === "Skills") {
+              return <MaterialCommunityIcons name="lightning-bolt" size={24} color={color} />
+              
+            }
+
+            // return <Ionicons name={iconName} size={25} color={color} />;
+            
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "#51dee0",
+          inactiveTintColor: "#c3fbfb",
+          style:{backgroundColor:'#001150',height:67,paddingBottom:8,borderWidth:3,borderColor:"#c3fbfb",borderRadius:40,
+          borderTopWidth: 3,borderTopColor:"#c3fbfb",marginLeft:5,marginRight:5,marginBottom:10}
+          
+        }}
+
+      
+      
+
+      >
+        <Tab.Screen name="Dashboard" component={Dashboard} />
+        <Tab.Screen name="ListOffers" component={Notifications}/>
+        <Tab.Screen name="Skills" component={Skills} />
+        <Tab.Screen name="Notification" component={Notifications} />
+        
+      </Tab.Navigator>
+
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  
+  return (
+    <NavigationContainer>
+
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+
+        
+        <Stack.Screen name='BottomNavigator' component={BottomNavigator} />
+        {/* <Stack.Screen name='Welcome1' component={Dashboard} />
+        <Stack.Screen name='Welcome2' component={Dashboard} />
+        <Stack.Screen name='CvPopover' component={Dashboard} />
+        <Stack.Screen name='Register' component={Dashboard} />
+        <Stack.Screen name='Dashboard' component={Dashboard} />
+        <Stack.Screen name='Skills' component={Dashboard} />
+        <Stack.Screen name='PersonalInfo' component={Dashboard} />
+        <Stack.Screen name='ListOffers' component={Dashboard} />
+        <Stack.Screen name='MyLikes' component={Dashboard} />
+        <Stack.Screen name='MyMissions' component={Dashboard} />
+        <Stack.Screen name='MyDocuments' component={Dashboard} />
+        <Stack.Screen name='MyContracts' component={Dashboard} />
+        <Stack.Screen name='MyPaySlips' component={Dashboard} />
+        <Stack.Screen name='MyWorkCertificates' component={Dashboard} />
+        <Stack.Screen name='MyOtherDocuments' component={Dashboard} /> */}
+        
+      </Stack.Navigator>
+
+    </NavigationContainer>
+  );
+}
---
+++ frontend/components/Dashboard.js
@@ -0,0 +1,113 @@
+import { StyleSheet, Text, View, Button, SafeAreaView, Dimensions,Image } from 'react-native'
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
+
+const Stack = createStackNavigator();
+
+export default function HomeScreen(props) {
+  return (
+    <SafeAreaView style={styles.container}>
+       <View>
+
+               <Text>Bonjour Maxine</Text>
+                <Image
+                   style={{
+                   width: 150,
+                   height: 150,
+                   borderRadius:150
+                  //  resizeMode: 'contain'
+                         }}
+                  source={require('../assets/girl.jpg')}
+                />
+                
+              </View>
+              
+      {/* <View style={styles.container}/> */}
+      
+      
+      <View >
+
+              <View style={styles.buttonContainer}>
+                <View>
+                {/* <Icon
+    name='fontawesome|facebook-square'
+    size={25}
+    color='#3b5998'
+    style={{height:25,width:25}}/> */}
+                <Button  style={{width:50}}
+                icon={
+                  <Icon
+                    name="arrow-right"
+                    size={15}
+                    color="white"
+                  />
+                }
+                title="Liste annonce"/>
+                </View>
+                
+                  <Button title="A etudier"/>
+              </View>
+
+              <View style={styles.buttonContainer}>
+                <View>
+                <Button title="Mes documents"/>
+                </View>
+                  <Button title="Info personnel"/>
+              </View>
+
+              <View style={styles.buttonContainer}>
+                <View>
+                <Button title="Skills"/>
+                </View>
+                  <Button title="Mission"/>
+              </View>
+
+       </View>
+      
+    </SafeAreaView>
+  )
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+flex: 1,
+backgroundColor: '#fff',
+// flexDirection:'row',
+justifyContent: 'center',
+alignItems: 'center',
+marginBottom: 0,
+position: 'absolute',
+top: 200,
+width: Dimensions.get("screen").width / 1,
+// flexWrap:"wrap"
+
+},
+
+  // width: Dimensions.get("screen").width / 2,
+  // height: 100,
+  // backgroundColor: "rgba(0,0,0,.5)",
+  // justifyContent: 'center',
+  // alignItems: 'center',
+  // position: 'relative',
+  // marginBottom: 1
+buttonContainer:{
+  flex:1,
+  width:500,
+  height: 100,
+  // backgroundColor: "rgba(0,0,0,.5)",
+  justifyContent: 'space-evenly',
+  alignItems: 'center',
+  // position: 'relative',
+  flexDirection:"row",
+  marginBottom:0,
+  borderRadius:5,
+  paddingBottom:100,
+  paddingTop:45
+
+}
+});
