5a43164fc2c2bfd0729b3ab83dd0bc798ac105e2
Nayeff BOINARIZIKI <nayeff.boinariziki@gmail.com>
2022-05-19 12:20:44 +0200

    save nayeff work

--- frontend/components/Inscription.js
+++ frontend/components/Inscription.js
@@ -46,6 +46,32 @@ export default function Inscription(props) {
           </Text>
           <Text style={{ color: "skyblue", margin: 50 }}>Femme</Text>
         </View>
+        <Input placeholder="BASIC INPUT" />
+
+        <Input
+          placeholder="INPUT WITH ICON"
+          leftIcon={{ type: "font-awesome", name: "chevron-left" }}
+        />
+
+        <Input
+          placeholder="INPUT WITH CUSTOM ICON"
+          leftIcon={<Icon name="user" size={24} color="black" />}
+        />
+
+        <Input
+          placeholder="Comment"
+          leftIcon={{ type: "font-awesome", name: "comment" }}
+          style={styles}
+          onChangeText={(value) => this.setState({ comment: value })}
+        />
+
+        <Input
+          placeholder="INPUT WITH ERROR MESSAGE"
+          errorStyle={{ color: "red" }}
+          errorMessage="ENTER A VALID ERROR HERE"
+        />
+
+        <Input placeholder="Password" secureTextEntry={true} />
       </View>
     </View>
   );
