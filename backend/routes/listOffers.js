a8b62d505dc77dc65c06e0d3d831685da60c1523
EddyDevFS <102260448+EddyDevFS@users.noreply.github.com>
2022-05-19 10:34:48 +0200

    ListCard/retourduBackOk/ComposantOfferCard

--- backend/routes/listOffers.js
+++ backend/routes/listOffers.js
@@ -5,10 +5,10 @@ const offerModel = require("../models/offers");
 /* GET users listing. */
 router.get("/listOffers", async function (req, res, next) {
   console.log("bonjour");
-  var offer = await offerModel.find();
+  var offers = await offerModel.find();
 
-  console.log("list offer", offer);
-  res.json({ offer });
+  console.log("list offers", offers);
+  res.json({ offers });
 });
 
 module.exports = router;
