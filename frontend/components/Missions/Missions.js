71279c81bd582d2babf43d3cd75fae74039eebaf
EddyDevFS <102260448+EddyDevFS@users.noreply.github.com>
2022-05-26 13:44:26 +0200

    eddy

--- frontend/components/Missions/Missions.js
+++ frontend/components/Missions/Missions.js
@@ -1,86 +1,86 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
-import React from 'react'
-import { Badge } from 'react-native-elements'
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import React from "react";
+import { Badge } from "react-native-elements";
 
-import { Feather } from '@expo/vector-icons'
-import { MaterialCommunityIcons } from '@expo/vector-icons'
-import { Ionicons } from '@expo/vector-icons'
-import { FontAwesome5 } from '@expo/vector-icons'
-import { FontAwesome } from '@expo/vector-icons'
+import { Feather } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 
-import AccordionMission from './AccordionMission'
+import AccordionMission from "./AccordionMission";
 
 export default function Missions() {
   const missionList = [
     {
-      title: 'Candidatures',
+      title: "Candidatures",
       icon: (
         <MaterialCommunityIcons
-          name='file-document-edit'
+          name="file-document-edit"
           size={40}
-          color='#B9FFFF'
+          color="#B9FFFF"
         />
       ),
-      notifications: 0,
-      items: [],
-    },
-    {
-      title: 'Offres reçues',
-      icon: <Ionicons name='contract' size={40} color='#B9FFFF' />,
       notifications: 4,
       items: [
         {
-          companyName: 'Facebook',
-          companyLogo: '',
-          jobTitle: 'Developpeur Front End',
-          dateSent: '12 Mai 2022',
-          timeSent: '10h05',
+          companyName: "Facebook",
+          companyLogo: "",
+          jobTitle: "Developpeur Front End",
+          dateSent: "12 Mai 2022",
+          timeSent: "10h05",
           stage: 1,
         },
         {
-          companyName: 'Facebook',
-          companyLogo: '',
-          jobTitle: 'Developpeur Front End',
-          dateSent: '12 Mai 2022',
-          timeSent: '10h05',
+          companyName: "Facebook",
+          companyLogo: "",
+          jobTitle: "Developpeur Front End",
+          dateSent: "12 Mai 2022",
+          timeSent: "10h05",
           stage: 2,
         },
         {
-          companyName: 'Facebook',
-          companyLogo: '',
-          jobTitle: 'Developpeur Front End',
-          dateSent: '12 Mai 2022',
-          timeSent: '10h05',
+          companyName: "Facebook",
+          companyLogo: "",
+          jobTitle: "Developpeur Front End",
+          dateSent: "12 Mai 2022",
+          timeSent: "10h05",
           stage: 3,
         },
         {
-          companyName: 'Facebook',
-          companyLogo: '',
-          jobTitle: 'Developpeur Front End',
-          dateSent: '12 Mai 2022',
-          timeSent: '10h05',
+          companyName: "Facebook",
+          companyLogo: "",
+          jobTitle: "Developpeur Front End",
+          dateSent: "12 Mai 2022",
+          timeSent: "10h05",
           stage: 4,
         },
       ],
     },
     {
-      title: 'Missions à venir',
-      icon: <FontAwesome5 name='business-time' size={40} color='#B9FFFF' />,
+      title: "Offres reçues",
+      icon: <Ionicons name="contract" size={40} color="#B9FFFF" />,
       notifications: 0,
       items: [],
     },
     {
-      title: 'Missions archivées',
-      icon: <FontAwesome name='archive' size={40} color='#B9FFFF' />,
+      title: "Missions à venir",
+      icon: <FontAwesome5 name="business-time" size={40} color="#B9FFFF" />,
       notifications: 0,
       items: [],
     },
-  ]
+    {
+      title: "Missions archivées",
+      icon: <FontAwesome name="archive" size={40} color="#B9FFFF" />,
+      notifications: 0,
+      items: [],
+    },
+  ];
 
   return (
     <ScrollView style={styles.body}>
       <View style={styles.container}>
-        <Feather name='briefcase' size={60} color='#B9FFFF' />
+        <Feather name="briefcase" size={60} color="#B9FFFF" />
         <Text style={styles.textTitle}>Mes Missions</Text>
         <View style={styles.missionOptions}>
           {missionList.map((missionOption, index) => {
@@ -92,30 +92,30 @@ export default function Missions() {
                 notifications={missionOption.notifications}
                 items={missionOption.items}
               />
-            )
+            );
           })}
         </View>
       </View>
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: '#000b33',
+    backgroundColor: "#000b33",
   },
   container: {
     flex: 1,
     marginTop: 50,
-    alignItems: 'center',
+    alignItems: "center",
   },
   missionOptions: {
     marginTop: 30,
     flex: 1,
-    width: '100%',
+    width: "100%",
   },
   textTitle: {
-    color: '#B9FFFF',
+    color: "#B9FFFF",
   },
-})
+});
